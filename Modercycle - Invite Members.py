c3ac34354d6a45a26c7d4bef9dd10fa934feb00f
Terry Brooks, Jr <Terry@ModeAnalytics.com>
2021-07-14 08:03:58 -0500

    Create Modercycle - Invite Members.py

---
+++ Modercycle - Invite Members.py
@@ -0,0 +1,41 @@
+import requests
+import pandas as pd
+
+from requests.auth import HTTPBasicAuth
+
+host = 'https://app.mode.com'
+org  = 'ENTER WORKSPACE NAME FOUND IN URL'
+# You will need to generate an API Token & Password
+api_token    = 'NEW API TOKEN'
+api_password = 'NEW API PASSWORD'
+
+headers = {
+	'Content-Type': 'application/json',
+	'Accept': 'application/hal+json'
+}
+url = '%s/api/%s/invites' % (host, org)
+
+def send_invite(email, message):
+	values = """
+		{
+			"invite": {
+				"invitee": { "email": "%s" },
+				"message": "%s"
+			}
+		}
+	""" % (email, message)
+	
+	r = requests.post(url, 
+		data=values, 
+		headers=headers, 
+		auth=HTTPBasicAuth(api_token, api_password)
+	)
+	
+	if r.status_code == 200:
+		print('%s successfully invited!' % email)
+	else:
+		print('ERROR: invite to %s failed.' % email)
+		print(r.text)
+		
+df = pd.read_csv("ENTER FILE PATH")
+df.apply(lambda x: send_invite(x['email'],x['invite_message']), axis=1)

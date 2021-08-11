013d7eedce5e2e90ba332d9462f006dc383ff7d6
Mode Solutions Sandbox <85627824+ModeSolutionsSandbox@users.noreply.github.com>
2021-08-11 07:21:56 -0400

    Update Zen of Modercycle Maintence - Bulk Create Groups.py

--- Zen of Modercycle Maintence - Bulk Create Groups.py
+++ Zen of Modercycle Maintence - Bulk Create Groups.py
@@ -1,3 +1,4 @@
+# Requires Python 3 and the following libraries 
 import json
 import requests
 import pandas as pd
@@ -8,7 +9,7 @@ from requests.auth import HTTPBasicAuth
 host = 'https://modeanalytics.com'
 # This is found in the subdomain of the URL of your Mode Workspace URL 
 org = 'YOUR WORKSPACE NAME'
-# You will need to generate an API Token & Password
+# You will need to generate an API Token & Password - Instructions on Mode Help Site. - https://mode.com/help/articles/api-reference/#generating-api-tokens
 un = 'API TOKEN'
 pw = 'API PASSWORD'
 
@@ -26,6 +27,6 @@ def create_group(group_name):
 	
 	
 	return result
-
-df = pd.read_csv("/Users/terrybrooks/Desktop/API_Bulk_Create.csv")
+# Update with the actual relative file path of the CSV with the list of members. CVS File should have the follwing columns: name, email, invite_message
+df = pd.read_csv("ENTER FILE PATH")
 df.apply(lambda x: create_group(x['group_name'],), axis=1)

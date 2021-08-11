0d68f0d9c58df1ceacac1a4e0f75e368722f144a
Mode Solutions Sandbox <85627824+ModeSolutionsSandbox@users.noreply.github.com>
2021-08-11 08:58:28 -0400

    Update and rename Grouping - Step 2- Assinging Members to .py to Modercycle - Auto Assign to Users to User Groups.py

--- Modercycle - Auto Assign to Users to User Groups.py
+++ Modercycle - Auto Assign to Users to User Groups.py
@@ -4,10 +4,10 @@ import pandas as pd
 from requests.auth import HTTPBasicAuth
 
 host = 'https://app.mode.com'
-org  = 'demo'
-
-api_token    = '3065fbd67935'
-api_password = '68c2d7f789437fa3acfbbbcb'
+org  = 'ENTER WORKSPACE NAME FOUND IN URL'
+# You will need to generate an API Token & Password - Instructions on Mode Help Site. - https://mode.com/help/articles/api-reference/#generating-api-tokens
+api_token    = 'NEW API TOKEN'
+api_password = 'NEW API PASSWORD'
 
 
 headers = {
@@ -34,7 +34,8 @@ def add_user_to_group(group_id, group_name, user_id, email):
     print('ERROR: Failed to add %s.' % email)
     print(r.json())
     
-df = pd.read_csv("/Users/terrybrooks/Desktop/Buckets.csv")
+# Update with the actual relative file path of the CSV with the list of members. CVS File should have the follwing columns: name, email, invite_message		
+df = pd.read_csv("ENTER FILE PATH")
 df.apply(lambda x: add_user_to_group(x['group_id'],x['group_name'],x['user_id'],x['email']), axis=1)
 
   
